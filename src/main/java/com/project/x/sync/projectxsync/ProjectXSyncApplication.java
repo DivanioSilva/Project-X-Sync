7eec037d6471f975bed3a1b410c80b3281e766fb
Divanio Silva <dcsilva@mail.ru>
2022-09-27 19:31:20 +0100

    Project setup

---
+++ src/main/java/com/project/x/sync/projectxsync/ProjectXSyncApplication.java
@@ -0,0 +1,13 @@
+package com.project.x.sync.projectxsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectXSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/x/sync/projectxsync/ProjectXSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.x.sync.projectxsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectXSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
